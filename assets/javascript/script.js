426a8d3b57a15f54afbb1ee8b977df932f0710b0
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-13 18:58:34 -0500

    added quryURL to take in city variable

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,6 +5,10 @@ $("#searchBtn").on("click", function (event){
     event.preventDefault()
     var city = $("#searchInput").val();
     console.log(city);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
+    + city + "&appid=e843562227bb1a74a27f6a81efac2153";
+
+    
 
 
 });
