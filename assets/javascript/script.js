687b4816492c428a5d0a4c3c88f4349263d18f39
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-13 19:38:06 -0500

    Added ajax to get queryURL and set the text values in the forecsat card to corresponing values from the api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,12 +6,37 @@ $("#searchBtn").on("click", function (event){
     var city = $("#searchInput").val();
     console.log(city);
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
-    + city + "&appid=e843562227bb1a74a27f6a81efac2153";
+    + city + "&units=imperial&appid=e843562227bb1a74a27f6a81efac2153";
+   
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function (response) {
+            var data = response;
+         console.log(data);
+       $("#city").text(data.name + " (" + moment().subtract(10, 'days').calendar() +")");
+       $("#temperature").text("Temperature: "+data.main.temp+"°F");
+       $("#humidity").text("Humidity: "+data.main.humidity+"%");
+       $("#windSpeed").text("WindSpeed: "+data.wind.speed+" MPH");
+
+       var long = data.coord.lon;
+       var lat = data.coord.lat;
+
+       queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" 
+       + lat + "&lon=" + long + "&appid=e843562227bb1a74a27f6a81efac2153";
+   
+       $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function (response) {
+            var uviData = response;
+            $("#uvIndex").text("UV Index: "+uviData.value);
+
+      })
+    });
 
     
-
-
-});
-
-
+  });
 });
